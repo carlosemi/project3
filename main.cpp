e434c6d1cd74b2d42a21bda206db49202743e309
carlosemi <carlos-egf@hotmail.com>
2020-10-07 20:24:33 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+
+using namespace std;
+
+class Algorithms{
+
+    public:
+        //This function is for selection sort
+        void selectionSort(int numbers[], int numbersSize){
+
+            int i = 0;
+            int j = 0;
+
+            int indexSmallest = 0;
+            int temp = 0;
+
+            for(i = 0; i < numbersSize; i++){
+
+                //Find index of smallest remaining element 
+                indexSmallest = i; 
+
+                for(j = i + 1; j < numbersSize; j++){
+
+                    if( numbers[j] < numbers[indexSmallest]){
+                        indexSmallest = j;
+                    }
+                }
+
+                //Swap numbers[i] and numbers[indexSmallest]
+                temp = numbers[i];
+                numbers[i] = numbers[indexSmallest];
+                numbers[indexSmallest] = temp;
+            }
+
+            //Print the sorted array
+            cout << "Selection Sort: ";
+            for(int z = 0; z < numbersSize; z++){
+
+                cout << numbers[z] << " ";
+            }
+
+            cout << endl;
+        }
+
+
+        void insertionSort(int numbers[], int numbersSize){
+            int i = 0;
+            int j = 0;
+
+            int temp = 0; //Temporary variable for swap
+
+            for(i = 1; i < numbersSize; i++){
+
+                j = i;
+                //Insert numbers[i] into sorted array
+                //stopping once numbers[i] in correct position
+
+                while(j > 0 && numbers[j] < numbers[j - 1] ){
+
+                    //Swap numbers[j] and numbers[j - 1]
+                    temp = numbers[j];
+                    numbers[j] = numbers[j - 1];
+                    numbers[j - 1] = temp;
+
+                    --j;
+                }
+            }
+
+            //Print the Insertion array
+            cout << "Insertion Sort: ";
+            for(int z = 0; z < numbersSize; z++){
+
+                cout << numbers[z] << " ";
+            }
+
+            cout << endl;
+        }
+
+
+        void shellSort(){
+
+        }
+
+
+        void quickSort(){
+
+        }
+
+
+        void mergeSort(){
+
+        }
+
+};
+
+int main(){
+
+    //cout << ("Starting Program") << endl;
+
+    int array [] = {4, 3, 8, 5, 6};
+
+    int size = sizeof(array) / sizeof(array[0]);
+    cout << "Size of array: " << size << endl;
+    cout << "Unsorted array: ";
+
+    for(int z = 0; z < 5; z++){
+
+                cout << array[z] << " ";
+    }
+
+    cout << endl;
+
+    Algorithms algo;
+
+    //algo.selectionSort(array, size);
+    algo.insertionSort(array, size);
+
+    
+}
